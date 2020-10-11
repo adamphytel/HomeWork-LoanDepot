d9d7adf12793b0dd2a4e41337594ba3a8359b06a
adamphytel <adam.guo@gmail.com>
2020-10-10 22:15:33 -0500

    Add files via upload
    This a function to find the first recurring character in a string.

---
+++ findFirstRecurringCharacter.ps1
@@ -0,0 +1,28 @@
+﻿Function get-first-duplicate{
+
+param ([string]$testString)
+$testString.ToCharArray()
+$RepeatCharacterArray=@()
+
+for ($i = 0; $i -le $testString.Length; $i++)
+{
+
+       for ($j =$i+1; $j -lt $testString.Length; $j++)
+       {
+            if ($testString[$i] -match $testString[$j])
+            {
+               $RepeatCharacterArray =$j
+                                
+            }
+            
+             
+           $firstRepeatPosition = ($RepeatCharacterArray  | Measure -Minimum).Minimum 
+           
+        }
+
+}
+write-host "The first recuring character is " $testString[$firstRepeatPosition]
+}
+
+
+get-first-duplicate('abcdedcba')
